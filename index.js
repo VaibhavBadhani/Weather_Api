97cabb9c273aad8ede94e6d98e402abd6c0d7e5a
Vaibhav Badhani <badhanivaibhav@gmail.com>
2024-07-02 21:43:05 +0530

    location access

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 const API_KEY = "3e888ebd826c69dc88f265549f584bd4";
 
+function showRenderedData(data){
+    let newPara = document.createElement('p');
+    newPara.textContent = '${data?.main?.temp.toFixed(2)} C'
+    document.body.appendChild(newPara); 
+}
 try{
     // let latitude = 15.3333;
     // let longitude = 74.0833;
@@ -11,12 +16,45 @@ try{
      
     console.log("weather details->  ", data);
 
-    // let newPara = document.createElement('p');
-    // newPara.textContent = '${data?.main?.temp.toFixed(2)} C'
-    // document.body.appendChild(newPara); 
+   
 
-    showRenderedData();
+    showRenderedData(data);
 }
 catch(e){
     console.log("EROOR");
+}
+
+
+function switchTab(clickedTab){
+    apiErrorContainer.classList.remove("active");
+
+    if(clickedTab!=currentTab){
+        currentTab.classList.remove("active");
+        currentTab = clickedTab;
+        currentTab.classList.add("current-tab");
+
+        if(!searchFrom.classList.contains("active")){
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.add("active");
+            searchForm.classList.add("active");
+        }
+        else{
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+        }
+    }
+}
+function getLocation(){
+    if(navigator.getLocation){
+        navigator.getLocation.getCurrentPosition(showPosition);
+    }
+    else{
+        console.log("donot support location");
+    }
+}
+function showPosition(position){
+    let lati = position.coords.latitude;
+    let long = position.coords.longitude;
+    console.log("latitude-> "+lati);
+    console.log("longitude-> "+long);
 }
